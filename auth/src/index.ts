a2668b5b58aa386151bf9e01af2af727074afa1b
Robert Hardy <hardyrar@gmail.com>
2021-08-07 19:29:49 +0100

    Add endpoint for getting the current user

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,11 @@ import {json} from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+    res.send("Hello :)")
+})
+
+
 app.listen(3000, () => {
     // tslint:disable-next-line:no-console
     console.log("Listening on port 3000")
