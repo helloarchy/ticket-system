e15c0473b71549ef371453cac36c5a4b0c8f43a5
Robert Hardy <hardyrar@gmail.com>
2021-08-08 20:00:30 +0100

    Add validation for signup body properties

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,7 +3,15 @@ import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post('/api/users/signup',(req: Request, res: Response) => {
+router.post('/api/users/signup', [
+    body('email')
+        .isEmail()
+        .withMessage("Email must be valid"),
+    body("password")
+        .trim()
+        .isLength({min: 4, max: 20})
+        .withMessage("Password must be between 4 and 20 characters")
+],(req: Request, res: Response) => {
     console.log("Hello again :)")
 });
 
