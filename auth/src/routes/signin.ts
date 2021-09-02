10c219072af3e0b77d4e76e7373694657799fc1c
Robert Hardy <hardyrar@gmail.com>
2021-09-02 19:58:07 +0100

    Check sign in credentials, store jwt if valid

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -3,6 +3,10 @@ import express, { Request, Response } from 'express';
 import { RequestValidationError } from '../errors/request-validation-error';
 
 import { validateRequest } from '../middleware/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -13,7 +17,35 @@ router.post(
     body('password').trim().notEmpty().withMessage('You must provide a password'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const isPasswordMatch = await Password.compare(existingUser.password, password);
+    if (!isPasswordMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store JWT in session
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signInRouter };
